5a68cbea684b89cf46e30b540f468de2b60a3675
varaprasadh <varaprasadh.a@gmail.com>
2019-03-30 23:01:06 +0530

    parser completed need to decorate

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ function parse(source,target){
     }
     var s_ext=source.slice(source.lastIndexOf('.')+1);
     var t_ext = target?target.slice(target.lastIndexOf('.') + 1):'';
-    console.log("extensions: ",s_ext,t_ext);
-
+    // console.log("extensions: ",s_ext,t_ext);
 
      if(s_ext=='csv'){
         //convert csv to json
-        var data=fs.readFileSync(path.join(__dirname,source),'utf8');
+        var data=fs.readFileSync(path.join(source),'utf8');
         // console.log(data);
         var rows=data.split('\n');
         // console.log(rows.length);
@@ -54,7 +53,7 @@ function parse(source,target){
     else{
       var jsonData = JSON.stringify(objects);
       console.log(`data writing to the file:${target}`);
-        fs.writeFile(path.join(__dirname, target), jsonData,(err)=>{
+        fs.writeFile(path.join(target), jsonData,(err)=>{
             if(err){
                 console.log("something went wrong..");
             }
@@ -63,16 +62,41 @@ function parse(source,target){
             }
         });
     }
-         
+
     }
     else{
        //convert json to csv
+       var jsonData=fs.readFileSync(path.join(source),'utf8');
+       //console.log(jsonData);
+       var jsonObj=JSON.parse(jsonData);
+       var headProps=Object.keys(jsonObj[0]);
+       var headrow=headProps.toString();
+    //    console.log(headrow);
+       var dataRows=[];
+       jsonObj.forEach(obj=>{
+         var row=[Object.values(obj)].toString();
+        //    console.log(row);
+           dataRows.push(row.concat('\n'));
+       })  
+        var csvData = headrow.concat('\n',dataRows.join('')); 
+     
+        if(!target){
+            console.log(csvData); 
+        }
+        else{
+            fs.writeFile(path.join(target),csvData,(err)=>{
+                if(err){
+                    console.log("something went wrong..");
+                }
+                else{
+                    console.log(`csv data has been succesfully written to the ${target} file`);
+                }
+            });
+        }
     }
-
-
 }
 function showJSON(objs){
-
+    
     objs.forEach(obj=>{
         console.log(JSON.stringify(obj),'\n');
     })
